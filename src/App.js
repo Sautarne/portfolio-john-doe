4287b465a79f0ef927e86e6e507dc8de84a6518b
Christophe PEREZ <christophe.perez690@gmail.com>
2024-03-27 21:45:21 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import Navigation from './components/general/Navigation';
+import Navigation from './components/general/Navigation.jsx';
+import Accueil from './pages/Accueil.jsx';
+import Services from './pages/Services.jsx';
+import Realisations from './pages/Realisations.jsx';
+import Blog from './pages/Blog.jsx';
+import MeContacter from './pages/Me contacter.jsx';
 
 function App() {
   return (  
     <div>
       <Navigation/>
+
+      <Routes>
+          <Route path='/' element={<Accueil/>}></Route>
+          <Route path='/services' element={<Services/>}></Route>
+          <Route path='/realisations' element={<Realisations/>}></Route>
+          <Route path='/blog' element={<Blog/>}></Route>
+          <Route path='/me-Contacter' element={<MeContacter/>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/general/Navigation.jsx
+++ src/components/general/Navigation.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {Routes, Route, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Navigation.css';
-import Accueil from '../../pages/Accueil.jsx';
-import Services from '../../pages/Services.jsx';
-import Realisations from '../../pages/Realisations.jsx';
-import Blog from '../../pages/Blog.jsx';
-import MeContacter from '../../pages/Me contacter.jsx';
+
 
 function Navigation () {
 
@@ -80,13 +76,7 @@ function Navigation () {
                     </div>
                 </div>
             </nav>
-            <Routes>
-                <Route path='/' element={<Accueil/>}></Route>
-                <Route path='/services' element={<Services/>}></Route>
-                <Route path='/realisations' element={<Realisations/>}></Route>
-                <Route path='/blog' element={<Blog/>}></Route>
-                <Route path='/me-Contacter' element={<MeContacter/>}></Route>
-            </Routes>
+
         </header>
     )
 }
