4943f593e5d05fb4dfed9a3a6801afb119dfc3d0
Christophe PEREZ <christophe.perez690@gmail.com>
2024-04-05 19:25:52 +0200

    Fin du la page service

--- src/components/general/Navigation.jsx
+++ src/components/general/Navigation.jsx
@@ -5,6 +5,7 @@ import './Navigation.css';
 
 function Navigation () {
 
+    // Permet de mettre la valeur "active" en fonction de la pages actuel
     const [activePage, setActivePage] = useState('accueil');
 
     const handleLinkClick = (page) => {
@@ -13,59 +14,59 @@ function Navigation () {
 
     return (
         <header>
-            <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-                <div class="container-fluid" id='nav'>
+            <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div className="container-fluid" id='nav'>
                     <Link
-                        class="navbar-brand"
+                        className="navbar-brand"
                         to="/"
                         onClick={() => handleLinkClick('accueil')}
                     >
                         John DOE
                     </Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
                                 <Link
-                                    class={`nav-link ${activePage === 'accueil' ? 'active' : ''}`}
+                                    className={`nav-link ${activePage === 'accueil' ? 'active' : ''}`}
                                     to="/"
                                     onClick={() => handleLinkClick('accueil')}
                                 >
                                     Accueil
                                 </Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link
-                                    class={`nav-link ${activePage === 'services' ? 'active' : ''}`}
+                                    className={`nav-link ${activePage === 'services' ? 'active' : ''}`}
                                     to="/services"
                                     onClick={() => handleLinkClick('services')}
                                 >
                                     Services
                                 </Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link
-                                    class={`nav-link ${activePage === 'realisations' ? 'active' : ''}`}
+                                    className={`nav-link ${activePage === 'realisations' ? 'active' : ''}`}
                                     to="/realisations"
                                     onClick={() => handleLinkClick('realisations')}
                                 >
                                     Réalisations
                                 </Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link
-                                    class={`nav-link ${activePage === 'blog' ? 'active' : ''}`}
+                                    className={`nav-link ${activePage === 'blog' ? 'active' : ''}`}
                                     to="/blog"
                                     onClick={() => handleLinkClick('blog')}
                                 >
                                     Blog
                                 </Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link
-                                    class={`nav-link ${activePage === 'me-contacter' ? 'active' : ''}`}
+                                    className={`nav-link ${activePage === 'me-contacter' ? 'active' : ''}`}
                                     to="/me-contacter"
                                     onClick={() => handleLinkClick('me-contacter')}
                                 >
---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import './Services.css'
+import ServicesType from '../../components/Services/Services-type';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDisplay, faFileCode , faMagnifyingGlassDollar} from "@fortawesome/free-solid-svg-icons";
+
+function Services () {
+
+    const [screenSize, setScreenSize] = useState(getScreenSize());
+
+    useEffect(() => {
+        function handleResize() {
+        setScreenSize(getScreenSize());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // récupère la taille de l'écran
+    function getScreenSize() {
+        if (window.matchMedia('(max-width: 767px)').matches) {
+        return 'mobile';
+        } else if (window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+        return 'tablet';
+        } else {
+        return 'desktop';
+        }
+    }
+
+    // Permet de changer l'image en fonction de la taille d'écran
+    let imageSource;
+    if (screenSize === 'mobile') {
+        imageSource = '../../../img/banner-mobile.jpg';
+    } else if (screenSize === 'tablet') {
+        imageSource = '../../../img/banner-tablette.jpg';
+    } else {
+        imageSource = '../../../img/banner.jpg';
+    }
+
+    return (
+        <div className='services-page'>
+            <img src={imageSource} alt="" />
+            <div className="services-titre">
+                <h2>
+                    Mon offre de services
+                </h2>
+                <h3>
+                    Voici les prestations sur lesquelles je peux intervenir.
+                </h3>
+                <div className='trait-bleu'></div>
+            </div>
+            <div className='services'>
+                <ServicesType
+                    titreServices="UX Design"
+                    texteServices="L'UX Design est une méthode de conception dentrée sur l'utilisateur. Son but est d'offrir une exérience de navigation optimale à l'internaute."
+                >
+                    <FontAwesomeIcon className='icon' icon={faDisplay}/>
+                </ServicesType>
+                    
+                <ServicesType
+                    titreServices="Développement Web"
+                    texteServices="Le déceloppement de sites web repose sur l'utilisation des langages HTML, CSS, Javascript et PHP."
+                >
+                    <FontAwesomeIcon className='icon' icon={faFileCode}/>
+                </ServicesType>
+                <ServicesType
+                    titreServices="UX Design"
+                    texteServices="Le référencement naturel d'un site, aussi appelé SEO, consiste à mettre des techniques en oeuvre pour améliorer sa position dans les résultats des moteurs de recherche."
+                >
+                    <FontAwesomeIcon className='icon' icon={faMagnifyingGlassDollar}/>
+                </ServicesType>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
